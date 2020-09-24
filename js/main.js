2307242d82725405f9e9a0f169bf40cadf045273
hyper_X_octane <shrinaveen6053@gmail.com>
2020-09-24 18:25:17 +0530

    this is my first commit after some alteration

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$('.video-mask').ready(function (){
+  $('.video-mask').css('height', '-1');
+});
+$('.explore').click(function (){
+  $('.title-video').css("transform","rotate(180deg) scale(1.6) translate(-18%,-6%)");
+  $('body').css("height","1200px");
+});
