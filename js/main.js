df447823103d81f162d36fa1990127885c4d78a1
hyper_X_octane <shrinaveen6053@gmail.com>
2020-09-24 19:46:42 +0530

    after addition of video

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ $('.video-mask').ready(function (){
   $('.text').css('right','24.2%');
   $('.text').css('opacity','100');
   $('.explore-delay').css({'transitionDelay':'3000','opacity':'100'});
+  $('.welcome').css('right','14.4%');
+  $('.welcome-sub').css('right','3.6%');
 });
 $('.explore').click(function (){
   $('.title-video').css("transform","rotate(180deg) scale(1.6) translate(-18%,-6%)");
