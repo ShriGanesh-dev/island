422a7e16510aa8e81b58736047fc64d169321710
hyper_X_octane <shrinaveen6053@gmail.com>
2020-09-25 11:40:18 +0530

    from left aligned elements

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
+
 $('.video-mask').ready(function (){
   $('.video-mask').css('height', '-1');
-  $('.text').css('right','24.2%');
+  $('.text1').css('left','690px');
+  $('.text2').css('left','865px');
   $('.text').css('opacity','100');
+
   $('.explore-delay').css({'transitionDelay':'3000','opacity':'100'});
-  $('.welcome').css('right','14.4%');
-  $('.welcome-sub').css('right','3.6%');
+  $('.welcome').css('left','914px');
+  $('.welcome-sub').css('left','920px');
 });
 $('.explore').click(function (){
   $('.title-video').css("transform","rotate(180deg) scale(1.6) translate(-18%,-6%)");
